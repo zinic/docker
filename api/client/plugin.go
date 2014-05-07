6369999e9f91d462f0e73d5ff29daf86ba44e865
John Hopper <john.hopper@rackspace.com>
2014-05-07 02:00:07 -0500

    code cleanup

--- api/client/plugin.go
+++ api/client/plugin.go
@@ -1,94 +1,68 @@
 package client
 
 import (
-	"fmt"
-	"io"
-	"net"
+    "fmt"
 )
 
 const (
-	CONTINUE = 0
-	RETURN   = 1
+    // Continuing the command allows the flow of the command to continue
+    // once the flow exits the plugin.
+    PLUGIN_CONTINUE_CMD = 0
+
+    // Exiting the command allows a plugin to cancel the flow of a command
+    // and returning to the user. This is useful in error conditions.
+    PLUGIN_EXIT_CMD   = 1
 )
 
-type CallDetails struct {
-	Method       string
-	Path         string
-	Data         interface{}
-	PassAuthInfo bool
-}
-
-type CliRemote interface {
-	Call(cli *DockerCli, callDetails *CallDetails) (io.ReadCloser, int, error)
-	Dial(cli *DockerCli) (net.Conn, error)
-}
-type CliRemoteInit func() (CliRemote, error)
-
+// A CliPlugin type implements two methods that reperesent call hooks that
+// may be executed either before or after the execution of the command
+// being invoked by the CLI user.
 type CliPlugin interface {
-	Before(cli *DockerCli, cmd string, args []string) (action int, err error)
-	After(cli *DockerCli, callError error, cmd string, args []string) (err error)
+    Before(cli *DockerCli, cmd string, args []string) (action int, err error)
+    After(cli *DockerCli, callError error, cmd string, args []string) (err error)
 }
+
+// A CliPlugin needs a portable initialization method to allow for easier
+// registration and management.
 type CliPluginInit func() (CliPlugin, error)
 
 var (
-	// All registered remotes
-	remotes map[string]CliRemoteInit
-
-	// All registered plugins
-	plugins map[string]CliPluginInit
+    // All registered plugins
+    plugins map[string]CliPluginInit
 )
 
 func init() {
-	remotes = make(map[string]CliRemoteInit)
-	plugins = make(map[string]CliPluginInit)
+    plugins = make(map[string]CliPluginInit)
 }
 
-func RegisterRemote(name string, initFunc CliRemoteInit) error {
-	if _, exists := remotes[name]; exists {
-		return fmt.Errorf("Name already registered %s", name)
-	}
-
-	remotes[name] = initFunc
-	return nil
-}
-
-func RegisterPlugin(name string, initFunc CliPluginInit) error {
-	if _, exists := plugins[name]; exists {
-		return fmt.Errorf("Name already registered %s", name)
-	}
-
-	plugins[name] = initFunc
-	return nil
-}
-
-func findRemote(name string) (CliRemoteInit, error) {
-	if initFunc, exists := remotes[name]; exists {
-		return initFunc, nil
-	}
-
-	return nil, fmt.Errorf("No such remote: %s", name)
-}
-
+// Finds a CliPlugin with a given name. If there is no regisitered plugin
+// with a matching name then an error is returned.
 func findPlugin(name string) (CliPluginInit, error) {
-	if initFunc, exists := plugins[name]; exists {
-		return initFunc, nil
-	}
+    if initFunc, exists := plugins[name]; exists {
+        return initFunc, nil
+    }
 
-	return nil, fmt.Errorf("No such plugin: %s", name)
+    return nil, fmt.Errorf("No such plugin: %s", name)
 }
 
-func NewCliRemote(name string) (CliRemote, error) {
-	if initFunc, err := findRemote(name); err == nil {
-		return initFunc()
-	} else {
-		return nil, err
-	}
+// Registers a CliPlugin in order to make it available for activation and
+// use.
+func RegisterPlugin(name string, initFunc CliPluginInit) error {
+    if _, exists := plugins[name]; exists {
+        return fmt.Errorf("Name already registered %s", name)
+    }
+
+    plugins[name] = initFunc
+    return nil
 }
 
+// Creates a new CliPlugin instance of the registered plugin with a name
+// matching the user supplied name argument. If no plugin is found with
+// a matching name, an error is returned.
 func NewCliPlugin(name string) (CliPlugin, error) {
-	if initFunc, err := findPlugin(name); err == nil {
-		return initFunc()
-	} else {
-		return nil, err
-	}
+    if initFunc, err := findPlugin(name); err == nil {
+        return initFunc()
+    } else {
+        return nil, err
+    }
 }
